763ff2c48506e705bc8717aca4f4f95a37be6044
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-27 21:57:31 +0530

    name and roll num

---
+++ CSPP2-Practice/module 1/Assignment-1/Solution.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Solution 
+{
+	public static void main(String[] args) 
+	{
+		Scanner reader = new Scanner(System.in);
+		String name = reader.nextLine();
+		long rollno = reader.nextLong();
+		System.out.println("Name : "+ name +", Roll Number : "+rollno);
+	}
+}
