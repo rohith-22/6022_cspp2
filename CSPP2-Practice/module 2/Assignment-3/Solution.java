00ab77707f4468e6a8b57a3a04587ec638a2e944
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-28 19:29:07 +0530

    "check_style_score -> 4 of 5 passed. style score: 0/1 "

---
+++ CSPP2-Practice/module 2/Assignment-3/Solution.java
@@ -0,0 +1,26 @@
+/**
+* impoting data from java utilities.
+*/import java.util.Scanner;
+/**
+*Class function
+*/
+public class Solution {
+	/*
+	Do not modify this main function.
+	*/
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);      
+        int base = s.nextInt();
+        int exponent = s.nextInt();
+        int result=power(base,exponent);
+        System.out.println(result);
+	}
+	/*
+	Need to write the power function and print the output.
+	*/
+	public static int power(int base, int exponent) {
+		if (exponent == 0)	return 1;
+		else if (exponent == 1) return base;
+		else return base * power(base, exponent-1);
+	}
+}
