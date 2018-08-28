111d01d9e75f0c01781c285263d3475bd65d0a3d
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-28 19:48:23 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-Practice/module 2/Assignment-3/Solution.java
+++ CSPP2-Practice/module 2/Assignment-3/Solution.java
@@ -34,11 +34,11 @@ public final class Solution {
     Need to write the power function and print the output.
     */
     public static long power(final int base, final int exponent) {
+        /**
+        * power recursive is defined here.
+        */
         if (exponent == 0) {
             return 1;
-        }
-        else if (exponent == 1) {
-            return base;
         }
         else {
             return base * power(base, exponent - 1);
