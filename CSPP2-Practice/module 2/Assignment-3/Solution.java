cb6670779098b2bd6719d1b007f580183633e323
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-28 19:43:16 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- CSPP2-Practice/module 2/Assignment-3/Solution.java
+++ CSPP2-Practice/module 2/Assignment-3/Solution.java
@@ -2,25 +2,39 @@
 * impoting data from java utilities.
 */import java.util.Scanner;
 /**
-*Class function
+*Class function.
 */
-public class Solution {
-	/*
-	Do not modify this main function.
-	*/
-	public static void main(String[] args) {
-		Scanner s=new Scanner(System.in);      
+public final class Solution {
+    /*
+    Do not modify this main function.
+    */
+    private Solution() {
+        /**
+        *private solution empty constructor.
+        */
+    }
+    /**
+    *main function begins here.
+    *@param args String
+    */
+    public static void main(final String[] args) {
+        /**
+        *input is taken here.
+        *@param base
+        *@param exponent
+        */
+        Scanner s = new Scanner(System.in);
         int base = s.nextInt();
         int exponent = s.nextInt();
-        long result=power(base,exponent);
+        long result = power(base, exponent);
         System.out.println(result);
-	}
-	/*
-	Need to write the power function and print the output.
-	*/
-	public static long power(int base, int exponent) {
-		if (exponent == 0)	return 1;
-		else if (exponent == 1) return base;
-		else return base * power(base, exponent-1);
-	}
+    }
+    /*
+    Need to write the power function and print the output.
+    */
+    public static long power(final int base,final int exponent) {
+        if (exponent == 0)  return 1;
+        else if (exponent == 1) return base;
+        else return base * power(base, exponent-1);
+    }
 }
