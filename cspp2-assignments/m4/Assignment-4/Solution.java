107ff669598aa6a8d64644f4ad7c7784f9eeb195
rohith-22 <rohithkottauppari@msitprogram.net>
2018-08-31 10:58:05 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m4/Assignment-4/Solution.java
+++ cspp2-assignments/m4/Assignment-4/Solution.java
@@ -1,24 +1,47 @@
 /**
- * { item_description }.
- */
+* { item_description }.
+*/
 import java.util.Scanner;
-public class Solution
-{/*
-	Do not modify the main function 
-	*/
-	public static void main(String[] args)
-	{
-		Scanner sc=new Scanner(System.in);
-		String s=sc.nextLine();
-		String reverse=reverseString(s);	
-		System.out.println(reverse);
-		
-	}
-	static String reverseString(final String s) {
-		String temp = "";
-		for (int i = 0; i < s.length(); i ++) {
-			temp = s.charAt(i) + temp;
-		}
-		return temp;
-	}  
+/**
+* Class for solution.
+*/
+public final class Solution {
+    /**
+    * Constructs the object.
+    */
+    private Solution() {
+        /**
+        * unused.
+        */
+    }
+    /**
+    * { function_description }.
+    *
+    * @param      args  The arguments
+    */
+    public static void main(final String[] args) {
+        /**
+        * { var_description }.
+        */
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        String reverse = reverseString(s);
+        System.out.println(reverse);
+    }
+    /**
+    * function.
+    * @param      s     { parameter_description }
+    *
+    * @return     { description_of_the_return_value }
+    */
+    static String reverseString(final String s) {
+        /**
+        * { var_description }.
+        */
+        String temp = "";
+        for (int i = 0; i < s.length(); i++) {
+            temp = s.charAt(i) + temp;
+        }
+        return temp;
+    }  
 }
