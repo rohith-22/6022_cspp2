9596015b59b8c9912848f27ad5bac1e2f6d7f90d
rohith-22 <rohithkottauppari@msitprogram.net>
2018-09-01 11:22:15 +0530

    "check_style_score -> 5 of 5 passed. style score: 0/1 "

--- cspp2-assignments/m6/Assignment-2/Solution.java
+++ cspp2-assignments/m6/Assignment-2/Solution.java
@@ -9,11 +9,11 @@ final class Solution {
   /**
    * Constructs the object.
    */
-  private static float HU_N = 100;
+  private static float HU_n = 100;
   /**
    * 100.
    */
-  private static int HUN_DERED = 100;
+  private static int HU_ndered = 100;
   /**
    * Constructs the object.
    */
@@ -37,8 +37,8 @@ final class Solution {
      */
     for (int i = 0; i < rows; i++) {
       for (int j = 0; j < columns; j++) {
-        int b = Math.round(a[i][j] / HU_N);
-        a[i][j] = b * HUN_DERED;
+        int b = Math.round(a[i][j] / HU_n);
+        a[i][j] = b * HU_ndered;
       }
     } return a;
   }
