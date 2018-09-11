7a283c9f7a2ec07be2fe4d0acd1bc6918b670acf
rohith-22 <rohithkottauppari@msitprogram.net>
2018-09-11 21:58:36 +0530

    "check_style_score -> 3 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m15/Assignment-1/Solution.java
+++ cspp2-assignments/m15/Assignment-1/Solution.java
@@ -7,47 +7,13 @@ class List {
    * final int number.
    */
   private final int num = 10;
-  //Implement all the methods mentioned to build a ListADT
-
   /**
-   * . The goal for the list is to store items. How are we going to store the
-   * items in the list? An array would be good. Right? So, assume we are only
-   * going to have ints in the list We need to create an array of ints to
-   * store the items added to the list.
-   *
-   * Create a variable of the type int[] Use the private access specifier Why
-   * private access specifier and why not public? Well, we don't want the
-   * array to be manipulated by methods that are outside the List class. If
-   * you allow methods outside the List class to manipulate the array then
-   * there is a possibility of having a corrupted list i.e., a list with
-   * incorrect items. This is not desirable and so having private access
-   * specifer will protect the array such corruption. This is a hard concept
-   * to understand. Discuss with your mentor.
+   * this is private list declaratiion.
    */
-
-  // declare a private int[]
-  // don't create the array yet using new
-  // that's the job of the List constructor
   private int[] list;
-
   /**
-   * . What are the other class variables needed for creating a list? How.
-   * about keeping track of the size of the list? If you add 2 items to the
-   * list then the size should be 2. Let's think about the size of the list by
-   * comparing it to the length of the array. Do they mean the same? No, Array
-   * length signifies the maximum number of items you can store in the list.
-   * Whereas, the size of the list denotes the number of items in the list.
-   * Makes sense? Here is an example: array = [1,2,3,0,0,0,0,0,0,0] The length
-   * of the array is 10 and size is 3. This means you can store 10 items in
-   * the list and currently it has 3 items. So, to keep track of the size we
-   * need a variable called size Again, we use private as we don't want that
-   * size variable to be accessed by the methods that are outside of the List
-   * class.
+   * this is privaet size declaration.
    */
-
-  // declare a private int size
-  // again, don't initialize it here
-  // variable initialization should be done in the constructor
   private int size;
 
   /**
@@ -56,15 +22,6 @@ class List {
    */
   public List() {
 
-    // what are the two variables to be initialized here? think about the
-    // private variables described above. What should be the default values?
-    // In the case of the list, it should be empty but it should be
-    // initialized with an array size like 10
-
-    // Think about the initial value for size.
-    // How many items do we have in the list when you create it?
-    // An empty list has how many items?
-    // That is the initial value to use for size.
     list = new int[num];
     size  = 0;
 
@@ -96,21 +53,12 @@ class List {
   public int size() {
     return size;
   }
-
   /**
-   * The remove method does what the name suggests. Removes an int item.
-   * specified by the index argument, from the list It also does an additional
-   * step. Think about what happens when an item is removed from the middle of
-   * the list It creates a hole in the list, right? This would mean, all the
-   * items that are to the right side of the removed item should be moved to
-   * the left by one position. Here is an example: array =
-   * [1,2,3,0,0,0,0,0,0,0] remove(1) would remove the item 2 which is at index
-   * position 1. But how do you remove the item from an array? Well, the way
-   * to remove it is to move all the items, that are to the right of the
-   * removed item, to the left So, the new array looks like this. array =
-   * [1,3,0,0,0,0,0,0,0,0] The method returns void (nothing)
+   * this function removes the element from given index.
    *
-   * @param      index  The index
+   * @param      index      The index
+   *
+   * @throws     Exception  throws a invalid position exception.
    */
   public void remove(final int index) throws Exception {
     if (index >= size || index < 0) {
@@ -121,37 +69,18 @@ class List {
     }
     size--;
   }
-
-
   /**
-   * Get method has to return the items that is at the index position passed.
-   * as an argument to the method. If the item doesn't exist then return a -1
-   * to indicate that there is no element at that index. How can an element
-   * not be there at a given position? Well, if the position is greater than
-   * the number of items in the list then that would mean the item doesn't
-   * exist. How do we check if the position is greater than the number of
-   * items in the list? Would size variable be useful?
+   * this is a getter function to get the last element from the list.
    *
    * @param      index  The index
    *
-   * @return     returns the index.
+   * @return     this returns the last element from the list.
    */
   public int get(final int index) {
     return list[index];
   }
-
   /**
-   * What happens when you print an object using println? Java provides a.
-   * method named toString that is internally invoked when an object variable
-   * is used in println. For example: List l = new List();
-   * System.out.println(l); This statement is a shortcut for
-   * System.out.println(l.toString());
-   *
-   * So, implement the toString method to display the items in the list in the
-   * square brackets notation. i.e., if the list has numbers 1, 2, 3 return
-   * the string [1,2,3] Caution: The array may be having other elements
-   * Example: [1,2,3,0,0,0,0,0,0,0] toString should only return the items in
-   * the list and not all the elements of the array.
+   * Returns a string representation of the object.
    *
    * @return     String representation of the object.
    */
@@ -201,8 +130,8 @@ class List {
     return -1;
   }
   /**
-   * { function_description }.
-   *
+   * this method is called when the size of the arrray is full.
+   * this method returns a array of double the size.
    * @return     returns the resize list.
    */
   public int[] resize() {
@@ -227,8 +156,7 @@ class List {
     }
   }
   /**
-   Removes all of its elements that are contained in the specified int array.
-
+  *Removes all of its elements that are contained in the specified int array.
    @param      newArray  The new array
   */
   public void removeAll(final int[] newArray) throws Exception {
@@ -285,6 +213,12 @@ class List {
     }
     return (cnt == size);
   }
+  /**
+   * this a add function which enables user to add the elements to array.
+   *this returns nothing.
+   * @param      index  The index
+   * @param      item   The item
+   */
   public void add(final int index, final int item) {
     if (index >= 0) {
       int[] b = Arrays.copyOfRange(list, 0, list.length);
@@ -308,6 +242,13 @@ class List {
     list = new int[num];
     size = 0;
   }
+  /**
+   * this function gives the count of the item.
+   *
+   * @param      item  The item
+   *
+   * @return     returns count.
+   */
   public int count(final int item) {
     int count = 0;
     for (int i = 0; i < size; i++) {
