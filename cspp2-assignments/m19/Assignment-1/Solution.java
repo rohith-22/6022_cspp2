ef21d1998e86b292837ce6b3ac43500941f0b301
rohith-22 <rohithkottauppari@msitprogram.net>
2018-09-15 12:08:22 +0530

    "check_style_score -> 0 of 13 passed. style score: 0/1 "

--- cspp2-assignments/m19/Assignment-1/Solution.java
+++ cspp2-assignments/m19/Assignment-1/Solution.java
@@ -138,7 +138,7 @@ class QuizOperations {
 		Quiz answers = new Quiz (choices);
 		for (int i = 0; i < answerCount; i++) {
 			System.out.println("question text " + (i + 1) + "(" + listObject.get(i).getAnswers(i) + ")");
-			System.out.println("choice 1" + "    choice 2" + "	choice 3" + "	choice 4");
+			System.out.println("choice 1" + "	choice 2" + "	choice 3" + "	choice 4"+"\n");
 		}
 
 	}
