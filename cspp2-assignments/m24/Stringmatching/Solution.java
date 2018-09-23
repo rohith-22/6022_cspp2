470d2d08859d231ba0785c609c567a80517949dd
rohith-22 <rohithkottauppari@msitprogram.net>
2018-09-23 20:27:23 +0530

    "check_style_score -> 0 of 3 passed. style score: 1/1 "

---
+++ cspp2-assignments/m24/Stringmatching/Solution.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.util.NoSuchElementException;
+import java.io.File;
+/**
+ * Class for solution.
+ */
+public final class Solution {
+  /**
+   * Constructs the object.
+   */
+  private Solution() {
+
+  }
+  /**
+   * main.
+   *
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+    StringMatching objBow = new StringMatching();
+    Scanner sc = new Scanner(System.in);
+    try {
+      File file = new File(sc.next());
+      File[] listOfFiles = file.listFiles();
+      objBow.words(listOfFiles);
+    } catch (NoSuchElementException e) {
+      System.out.println("empty directory");
+    }
+  }
+}
---
+++ cspp2-assignments/m24/Stringmatching/StringMatching.java
@@ -0,0 +1,80 @@
+import java.util.*;
+import java.io.*;
+public class StringMatching {
+	StringMatching() {
+
+	}
+	public void words(File[] list) {
+		ArrayList<String> text = new ArrayList<>();
+		File[] listOfFiles = list;
+		if (listOfFiles.length == 0) {
+			System.out.println("empty directory");
+		} else {
+			Arrays.sort(listOfFiles);
+			try {
+				System.out.print("      \t\t");
+				for (File eachFile : listOfFiles) {
+					System.out.print("\t" + eachFile.getName());
+					Scanner sc1 = new Scanner(eachFile);
+					String str = "";
+					while (sc1.hasNext()) {
+						str += sc1.nextLine() + " ";
+					}
+					text.add(str);
+				}
+			} catch (FileNotFoundException e) {
+				System.out.println(e.getMessage());
+			}
+			double temp = 0;
+			String tempStrOne = "";
+			String tempStrTwo = "";
+
+			System.out.println();
+			for (int i = 0; i < text.size(); i++) {
+				System.out.print(listOfFiles[i].getName());
+				for (int j = 0; j < text.size(); j++) {
+					System.out.print("\t\t" + findLCS(text.get(i), text.get(j)));
+					if (findLCS(text.get(i), text.get(j)) > temp
+					        && findLCS(text.get(i), text.get(j)) != 100) {
+						temp = findLCS(text.get(i), text.get(j));
+						tempStrOne = listOfFiles[i].getName();
+						tempStrTwo = listOfFiles[j].getName();
+					}
+				}
+				System.out.println();
+			}
+			System.out.println("Maximum similarity is between " + tempStrOne + " and " + tempStrTwo);
+			// System.out.println(text.get(0));
+		}
+	}
+	public double findLCS(String firstString, String secondString) {
+		int lengthOne = firstString.length();
+		int lengthTwo = secondString.length();
+		double totalLength = lengthOne + lengthTwo;
+		int[][] tempMatrix = new int[lengthOne + 1][lengthTwo + 1];
+		double result = 0;
+		double lcsValue = 0;
+		for (int i = 0; i <= lengthOne; i++) {
+			for (int j = 0; j <= lengthTwo; j++) {
+				if (i == 0 || j == 0) {
+					tempMatrix[i][j] = 0;
+				} else if (firstString.charAt(i - 1) == secondString.charAt(j - 1)) {
+					tempMatrix[i][j] = tempMatrix[i - 1][j - 1] + 1;
+				} else {
+					tempMatrix[i][j] = 0;
+				}
+				if (result < tempMatrix[i][j]) {
+					result = tempMatrix[i][j];
+				}
+			}
+		}
+		lcsValue = ((result * 2) / totalLength) * 100;
+		// System.out.println((int)lcsValue);
+		return Math.round(lcsValue * 100D) / 100;
+	}
+
+}
+
+
+
+// }
