59cac39419f2edacabc96b0aa4aff82cbda9b7c8
rohith-22 <rohithkottauppari@msitprogram.net>
2018-09-23 20:12:57 +0530

    "check_style_score -> 1 of 3 passed. style score: 0/1 "

--- cspp2-assignments/m24/Bagofwords/Solution.java
+++ cspp2-assignments/m24/Bagofwords/Solution.java
@@ -4,27 +4,26 @@ import java.io.*;
  * Class for solution.
  */
 public class Solution {
-	/**
-	 * Constructs the object.
-	 */
-	private Solution() {
+  /**
+   * Constructs the object.
+   */
+  private Solution() {
 
-	}
-	/**
-	 * main.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		BagOfWords objBow = new BagOfWords();
-		Scanner sc = new Scanner(System.in);
-		try {
-			File file = new File(sc.next());
-			File[] listOfFiles = file.listFiles();
-			objBow.words(listOfFiles);
-		}
-		catch (NoSuchElementException e) {
-			System.out.println("empty directory");
-		} 
-	}
+  }
+  /**
+   * main.
+   *
+   * @param      args  The arguments
+   */
+  public static void main(final String[] args) {
+    BagOfWords objBow = new BagOfWords();
+    Scanner sc = new Scanner(System.in);
+    try {
+      File file = new File(sc.next());
+      File[] listOfFiles = file.listFiles();
+      objBow.words(listOfFiles);
+    } catch (NoSuchElementException e) {
+      System.out.println("empty directory");
+    }
+  }
 }
