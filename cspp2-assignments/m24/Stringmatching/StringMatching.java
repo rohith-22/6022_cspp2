299ba42d0eb6a6e6bbe8108f84508de2bbc64b62
rohith-22 <rohithkottauppari@msitprogram.net>
2018-09-23 22:00:52 +0530

    "check_style_score -> 1 of 3 passed. style score: 1/1 "

--- cspp2-assignments/m24/Stringmatching/StringMatching.java
+++ cspp2-assignments/m24/Stringmatching/StringMatching.java
@@ -12,9 +12,9 @@ public class StringMatching {
 		} else {
 			Arrays.sort(listOfFiles);
 			try {
-				System.out.print("             ");
+				System.out.print("         ");
 				for (File eachFile : listOfFiles) {
-					System.out.print("\t" + eachFile.getName());
+					System.out.print("    " + eachFile.getName());
 					Scanner sc1 = new Scanner(eachFile);
 					String str = "";
 					while (sc1.hasNext()) {
